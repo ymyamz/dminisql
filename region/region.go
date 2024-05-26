0962a9c55e83f9b1d122e2323fec459ef3631a11
ymyamz <1436907265@qq.com>
2024-05-26 19:33:04 +0800

    sql

--- region/region.go
+++ region/region.go
@@ -20,12 +20,9 @@ type Region struct {
 	hostIP string
 	backupIP string
 	backupClient *rpc.Client
-	fu           FtpUtils
 
 }
 func (region *Region) Init() {
-
-	region.fu.Construct()
 
 	region.hostIP = region.foundhostIP()
 	var err error
