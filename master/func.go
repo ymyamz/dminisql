556bd147ddf097ff3f7311b58fdc7cd59d9b1286
multydoffer <whaterveranyway@gmail.com>
2024-05-26 16:46:26 +0800

    fix

--- master/func.go
+++ master/func.go
@@ -7,71 +7,72 @@ import (
 	"strings"
 )
 
-func (m *Master) CallTest(arg string, reply *string) error {  
-	fmt.Println("CALL master SUCCESS")  
-	*reply = "hello " + arg  
-	return nil  
-}  
+func (m *Master) CallTest(arg string, reply *string) error {
+	fmt.Println("CALL master SUCCESS")
+	*reply = "hello " + arg
+	return nil
+}
 
-//直接查看owntablelsit查询所有region的table，以表格形式返回所有table以及其所属regionip
-func (m *Master) TableShow(arg string, reply *string) error {  
+// 直接查看owntablelsit查询所有region的table，以表格形式返回所有table以及其所属regionip
+func (m *Master) TableShow(arg string, reply *string) error {
 	fmt.Println("master tableshow.called")
 	m.check_and_reset_Regions()
 	var res string
-	res="|"+ fmt.Sprintf(" %-15s |", "name")+ fmt.Sprintf(" %-15s |", "region_ip")+"\n"
-	res+="|-----------------|-----------------|\n"
+	res = "|" + fmt.Sprintf(" %-15s |", "name") + fmt.Sprintf(" %-15s |", "region_ip") + "\n"
+	res += "|-----------------|-----------------|\n"
 
-	for _,region_ip := range m.regionip_list{
-		tables:=*m.owntablelist[region_ip]
+	for _, region_ip := range m.regionip_list {
+		tables := *m.owntablelist[region_ip]
 		m.busy_operation_num[region_ip] += 1
-		for _,table := range tables{
-			res+="|"+ fmt.Sprintf(" %-15s |", table)+ fmt.Sprintf(" %-15s |", region_ip)+"\n"
+		for _, table := range tables {
+			res += "|" + fmt.Sprintf(" %-15s |", table) + fmt.Sprintf(" %-15s |", region_ip) + "\n"
 		}
 	}
-	*reply=res
+	*reply = res
 	return nil
 
 }
 
-func (master *Master)TableCreate(input string, reply *string)  error {  
+func (master *Master) TableCreate(input string, reply *string) error {
 	fmt.Println("master tablecreate.called")
 	master.check_and_reset_Regions()
-	items:=strings.Split(input, " ")
-	table_name:=items[2]  
+	items := strings.Split(input, " ")
+	table_name := items[2]
 	_, found := master.tableIP[table_name]
 	if found {
 		*reply = "table already exists"
-	}else {
+	} else {
 		//寻找table数最少的节点
 		min, best := math.MaxInt, ""
 		for ip, pTables := range master.owntablelist {
-			if len(*pTables) < min && master.busy_operation_num[ip]<util.BUSY_THRESHOLD{
+			if len(*pTables) < min && master.busy_operation_num[ip] < util.BUSY_THRESHOLD {
 				min, best = len(*pTables), ip
 			}
 		}
 
-		rpcRegion:=master.regionClients[best]
-		fmt.Println("best_ip:",best)
+		rpcRegion := master.regionClients[best]
+		fmt.Println("best_ip:", best)
 		master.busy_operation_num[best] += 1
 
 		var res string
 		//创建表
 		err := rpcRegion.Go("Region.Execute", input, &res, nil)
-		if err!= nil {
-			fmt.Println("region return err ",err)
+		if err != nil {
+			fmt.Println("region return err ", err)
 		}
 		master.tableIP[table_name] = best
 		util.AddToSlice(master.owntablelist[best], table_name)
 		*reply = "table created in region " + best
 	}
-	fmt.Println("region return ",*reply)
+	fmt.Println("region return ", *reply)
 	return nil
 }
-//test
-func (master *Master)QueryReigon(input string, reply *string)  error {  
+
+// test
+func (master *Master) QueryReigon(input string, reply *string) error {
 	fmt.Println("master.query called")
 	// TODO Change the ip
-	rpcRegion:=master.regionClients["localhost"]
+	rpcRegion := master.regionClients["localhost"]
 	master.busy_operation_num["localhost"] += 1
 	master.check_and_reset_Regions()
 	var res string
@@ -81,9 +82,9 @@ func (master *Master)QueryReigon(input string, reply *string)  error {
 		fmt.Println("SYSTEM HINT>>> timeout, region down!")
 	}
 	if call.Error != nil {
-		fmt.Println("RESULT>>> failed ",call.Error)
+		fmt.Println("RESULT>>> failed ", call.Error)
 	} else {
-		fmt.Println("RESULT>>> res: \n",res)
+		fmt.Println("RESULT>>> res: \n", res)
 	}
 	*reply = res
 
@@ -92,42 +93,42 @@ func (master *Master)QueryReigon(input string, reply *string)  error {
 
 // TableDrop 方法用于在分布式系统中删除表
 func (master *Master) TableDrop(input string, reply *string) error {
-    fmt.Println("master tabledrop.called")
-    
-    // 解析输入命令，获取要删除的表名
-    items := strings.Split(input, " ")
-    table_name := items[2]
-    
-    // 检查要删除的表是否存在
-    _, found := master.tableIP[table_name]
-    if !found {
-        *reply = "table doesn't exist"
-    } else {
-        // 获取要删除表的服务器 IP 地址
-        ip := master.tableIP[table_name]
-        rpcRegion := master.regionClients[ip]
-        var res string
-        
-        // 调用远程过程执行 SQL 命令
-        call, err := util.TimeoutRPC(rpcRegion.Go("Region.Execute", input, &res, nil), util.TIMEOUT_M)
-        if err != nil {
-            fmt.Println("region return err ", err)
-            return err
-        }
-        
-        // 检查远程过程调用是否成功
-        if call.Error != nil {
-            fmt.Println("%v region process table drop failed", ip)
-            return call.Error
-        }
-        
-        // 删除表，并更新数据结构
-        master.deleteTable(table_name, ip)
-        return nil
-    }
-    
-    fmt.Println("region return ", *reply)
-    return nil
+	fmt.Println("master tabledrop.called")
+
+	// 解析输入命令，获取要删除的表名
+	items := strings.Split(input, " ")
+	table_name := items[2]
+
+	// 检查要删除的表是否存在
+	_, found := master.tableIP[table_name]
+	if !found {
+		*reply = "table doesn't exist"
+	} else {
+		// 获取要删除表的服务器 IP 地址
+		ip := master.tableIP[table_name]
+		rpcRegion := master.regionClients[ip]
+		var res string
+
+		// 调用远程过程执行 SQL 命令
+		call, err := util.TimeoutRPC(rpcRegion.Go("Region.Execute", input, &res, nil), util.TIMEOUT_M)
+		if err != nil {
+			fmt.Println("region return err ", err)
+			return err
+		}
+
+		// 检查远程过程调用是否成功
+		if call.Error != nil {
+			fmt.Println("%v region process table drop failed", ip)
+			return call.Error
+		}
+
+		// 删除表，并更新数据结构
+		master.deleteTable(table_name, ip)
+		return nil
+	}
+
+	fmt.Println("region return ", *reply)
+	return nil
 }
 
 func (master *Master) deleteTable(table, ip string) {
@@ -136,16 +137,16 @@ func (master *Master) deleteTable(table, ip string) {
 	util.DeleteFromSlice(master.owntablelist[ip], table)
 }
 
-func (master *Master)check_and_reset_Regions()  error {
+func (master *Master) check_and_reset_Regions() error {
 	all_busy := true
-	for _,region_ip := range m.regionip_list{
-		if m.busy_operation_num[region_ip] < util.BUSY_THRESHOLD{
+	for _, region_ip := range master.regionip_list {
+		if master.busy_operation_num[region_ip] < util.BUSY_THRESHOLD {
 			all_busy = false
 		}
 	}
-	if all_busy{
-		for _,region_ip := range m.regionip_list{
-			m.busy_operation_num[region_ip] = 0
+	if all_busy {
+		for _, region_ip := range master.regionip_list {
+			master.busy_operation_num[region_ip] = 0
 		}
 	}
 	return nil
--- master/master.go
+++ master/master.go
@@ -14,37 +14,38 @@ import (
 type Master struct {
 	regionCount int
 
-	etcdClient    *clientv3.Client
+	etcdClient *clientv3.Client
 	//ip->client
 	regionClients map[string]*rpc.Client
 	//ip含有的table列表
-	owntablelist map[string]*[]string  // ip->tables
+	owntablelist map[string]*[]string // ip->tables
 	//该table所在region的ip
-	tableIP      map[string]string // table->ip
+	tableIP map[string]string // table->ip
 
-	backup	map[string]string // region server ip -> backup server ip
+	backup        map[string]string // region server ip -> backup server ip
 	regionip_list []string
 
 	busy_operation_num map[string]int // operations for each region in 1 minute, > BUSY_THRESHOLD deemed as busy
 
 }
-func (master *Master) Init(mode string){
+
+func (master *Master) Init(mode string) {
 	//便于本地测试
-	if mode=="d"{
-		master.regionip_list=util.Region_IPs
-	}else{
-		master.regionip_list=util.Region_IPs_LOCAL
+	if mode == "d" {
+		master.regionip_list = util.Region_IPs
+	} else {
+		master.regionip_list = util.Region_IPs_LOCAL
 	}
-	master.regionCount=len(master.regionip_list)
+	master.regionCount = len(master.regionip_list)
 	// etcd client init
 	// wait for update
 
 	//code阶段，先对region进行初始化，后续再进行优化
 	//遍历每一个region_ips，建立rpc连接
-	master.regionClients=make(map[string]*rpc.Client)
-	for _,region_ip := range master.regionip_list{
+	master.regionClients = make(map[string]*rpc.Client)
+	for _, region_ip := range master.regionip_list {
 		client, err := rpc.DialHTTP("tcp", region_ip+util.REGION_PORT)
-		if err!= nil {
+		if err != nil {
 			fmt.Println("master error >>> region rpc dial error:", err)
 			return
 		}
@@ -54,40 +55,38 @@ func (master *Master) Init(mode string){
 	}
 
 	//初始化ip含有的table列表
-	master.owntablelist=make(map[string]*[]string)
-	for _,region_ip := range master.regionip_list{
-		master.owntablelist[region_ip]=&[]string{}
+	master.owntablelist = make(map[string]*[]string)
+	for _, region_ip := range master.regionip_list {
+		master.owntablelist[region_ip] = &[]string{}
 	}
 
 	//初始化该table所在region的ip
 	//TODO
-	master.tableIP=make(map[string]string)
+	master.tableIP = make(map[string]string)
 	master.InitTableIP()
 
 	master.backup = make(map[string]string)
-	
+	master.busy_operation_num = make(map[string]int)
 }
 
-func (master *Master) Run(){
+func (master *Master) Run() {
 	fmt.Println("master init and listening ")
 	//初始化etcd集群
-	// var err error 
+	// var err error
 	// master.etcdClient, err= clientv3.New(clientv3.Config{
 	// 	Endpoints:   []string{util.ETCD_ENDPOINT},
 	// 	DialTimeout: 1 * time.Second,
 	// })
-	// if err != nil {  
-	// 	fmt.Printf("master error >>> etcd connect error: %v", err)  
-	// }  
+	// if err != nil {
+	// 	fmt.Printf("master error >>> etcd connect error: %v", err)
+	// }
 	// defer master.etcdClient.Close()
-
-
 
 	// 注册rpc函数
 	rpc.Register(master)
 	rpc.HandleHTTP()
 	// 启动server
-	l, err := net.Listen("tcp",  util.MASTER_PORT)
+	l, err := net.Listen("tcp", util.MASTER_PORT)
 	if err != nil {
 		fmt.Println("Accept error:", err)
 	}
@@ -98,10 +97,10 @@ func (master *Master) Run(){
 
 }
 
-//把本地的db文件中的table信息同步
-func (master *Master)InitTableIP(){
-	for _,region_ip := range master.regionip_list{
-		client:=master.regionClients[region_ip]
+// 把本地的db文件中的table信息同步
+func (master *Master) InitTableIP() {
+	for _, region_ip := range master.regionip_list {
+		client := master.regionClients[region_ip]
 
 		var res []string
 		call, err := util.TimeoutRPC(client.Go("Region.TableName", "no use", &res, nil), util.TIMEOUT_M)
@@ -109,28 +108,26 @@ func (master *Master)InitTableIP(){
 			fmt.Println("SYSTEM HINT>>> timeout, region down!")
 		}
 		if call.Error != nil {
-			fmt.Println("RESULT>>> failed ",call.Error)
+			fmt.Println("RESULT>>> failed ", call.Error)
 		} else {
-			fmt.Println("RESULT>>> res: \n",res)
+			fmt.Println("RESULT>>> res: \n", res)
 		}
 		//打印返回的table列表
-		fmt.Println("region_ip:",region_ip,"table list:",res)
+		fmt.Println("region_ip:", region_ip, "table list:", res)
 		//更新本地的tableIP和owntablelist
-		for _,table := range res{
-			master.tableIP[table]=region_ip
+		for _, table := range res {
+			master.tableIP[table] = region_ip
 		}
-		master.owntablelist[region_ip]=&res
+		master.owntablelist[region_ip] = &res
 	}
 
 }
 
-
-//询问master某个table在哪个ip的region中
-func (master *Master)GetTableIP(table string,reply *string) error{
-	if _,ok:=master.tableIP[table];!ok{
-		*reply=""
+// 询问master某个table在哪个ip的region中
+func (master *Master) GetTableIP(table string, reply *string) error {
+	if _, ok := master.tableIP[table]; !ok {
+		*reply = ""
 	}
-	*reply= master.tableIP[table]
+	*reply = master.tableIP[table]
 	return nil
 }
-
